7cc69a72ea8e84744cf194ae976eb044a2d4f75d
Damian-VA <damian.villasenor@gmail.com>
2020-11-09 13:47:56 -0600

    Damián began R map Kim Gilbert tutorial

--- Analisis/MosquitoReview.R
+++ Analisis/MosquitoReview.R
@@ -1,6 +1,25 @@
 # Analyses of "Mosquito bloodmeal preference 
-# in disturbed vs. wild landscapes (a review)
+# in disturbed vs. wild landscapes (a review)"
 
 # Guadalupe López-Nava
-# César A. Sandoval-Ruiz
 # Damián Villaseñor-Amador
+# César A. Sandoval-Ruiz
+
+####Fooling around####
+
+#Packages
+library(maps)    #map functions
+library(mapdata) #basic world map data
+
+#Canada map
+#Coordinates in decimals, R won't read
+#degrees, minutes and seconds
+map("worldHires","Canada",
+  xlim=c(-141,-53), #longitude
+  ylim=c(40,85),    #latitude
+  col="gray90",
+  fill=T)
+
+
+
+
